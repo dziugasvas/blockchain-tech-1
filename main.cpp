c3e4564dd12737d81d89f45a5f2b66197db79972
dziugasvas <dziugasvas@gmail.com>
2026-09-24 22:02:05 +0300

    Pradinis hash algoritmas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdint>
 
 using std::string;
 using std::cout;
@@ -8,7 +9,7 @@ using std::ifstream;
 using std::endl;
 using std::cin;
 
-bool readFile (const string& filename) {
+bool readFile (const string& filename, string& content) {
 
     ifstream file(filename);
 
@@ -20,7 +21,7 @@ bool readFile (const string& filename) {
     char c;
 
     while (file.get(c)) {
-        cout << c;
+        content += c;
     }
 
     file.close();
@@ -29,10 +30,18 @@ bool readFile (const string& filename) {
 
 }
 
+void hashFunction(const string& input) {
+    uint32_t state[8];
+
+    for (int i = 0; i < 8; i++) {
+        state[i] = 0;
+    }
+}
 
 int main() {
 
     string filename;
+    string content;
 
     cout << "Turimi .txt failai: " << endl;
     system("ls *.txt");
@@ -40,10 +49,14 @@ int main() {
     cout << "Iveskite failo pavadinima: ";
     cin >> filename;
 
-    if (!readFile(filename)) {
+    if (!readFile(filename, content)) {
         return 1;
     }
-    
 
+    cout << "Nuskaitytas turinys:" << endl;
+    cout << content << endl;
+
+    hashFunction(content);
+    
     return 0;
 }
